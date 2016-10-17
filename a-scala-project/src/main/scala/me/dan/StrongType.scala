214ec62ff090a258c102dffd6c7268a44e4c8ecf
dqsgit <kernelonsteroids@gmail.com>
2016-10-17 18:25:32 +0800

    added comments on StrongType.scala

--- a-scala-project/src/main/scala/me/dan/StrongType.scala
+++ a-scala-project/src/main/scala/me/dan/StrongType.scala
@@ -1,16 +1,23 @@
 package me.dan
 
 
-
+/**
+ * Demo of utilizing scala less verbosity
+ * in coding to implement detailed
+ * strongly type codes
+ */
 object StrongType extends App {
 
+  /** supported types of a field */
   abstract class ColumnType(val name: String)
   final object IntColumn extends ColumnType("Int")
   final object StringColumn extends ColumnType("String")
 
+  /** metadata of the source class */
   final case class Table(val name: String)
   final case class Field(val name: String, val columnType: ColumnType)
 
+  /** the actual signature of a source class */
   trait DBTableSource {
     val table: Table
     val fields: Array[Field]
@@ -37,15 +44,19 @@ object StrongType extends App {
   MySampleDBTable.describe
 
 
-  /** what if we have some implicit helpers */
+  /** what if we have some implicit helpers to construct the source class */
   sealed trait CreateTable {
     val tbl: Table
   }
 
-  object DBTableSourceImplicitis {
+  object DBTableSourceImplicits {
     import scala.collection.mutable.ArrayBuffer
+
+    /** an implicit function convert automatically
+        tuple of String, ColumnType to Field */
     implicit def tupleToField(kv: (String, ColumnType)) = Field(kv._1, kv._2)
 
+    /** from a string, we can start off the object composition */
     implicit def wrapToCreateTable(s: String) = new CreateTable {
       val tbl = Table(s)
       private val flds = new ArrayBuffer[Field]()
@@ -61,26 +72,33 @@ object StrongType extends App {
     }
   }
 
-  import DBTableSourceImplicitis._
+  /** expose the implicit functions */
+  import DBTableSourceImplicits._
+  /** let's create the object */
   val mySampleTDBTable = "implicit_table" << ("id", IntColumn) << ("name", StringColumn) build
 
+  /** then trying to describe it */
   mySampleTDBTable.describe
 
 
-  /** --- or better yet --- */
+  /** or better yet as a function */
   def createTableSource(name: String)(flds: (String, ColumnType)*) = new DBTableSource {
     val table = Table(name)
     val fields = flds.toArray.map(kv => Field(kv._1, kv._2))
   }
-
+  /** let's create the object */
   val createdTableSource = createTableSource("functional_table")(
       "id" -> IntColumn,
       "name" -> StringColumn
     )
 
+  /** then trying to describe it */
   createdTableSource describe
 
-  println("Demo of StrongType")
+  /**
+   * With this, we were able to implement detailed facilities without much of
+   * verbosity in coding since scala tookout unnecessary syntax
+   */
 
 
 }
