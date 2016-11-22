e7ab213636cef3faf51d2c333378528b89df1b8d
dansuzuki <kernelonsteroids@gmail.com>
2016-11-22 19:52:25 +0800

    added FuturePool on WordCountExecutor

--- spark-finagle/src/main/scala/me/dan/finagle/Test.scala
+++ spark-finagle/src/main/scala/me/dan/finagle/Test.scala
@@ -1,20 +1,24 @@
 package me.dan.finagle
 
 
-import com.twitter.util.{Await, Future, Promise}
+import com.twitter.util.{Await, Future, FuturePool, Promise}
+import java.util.concurrent.{Future => JFuture, _}
+
 
 object Test extends App {
+  val executor = Executors.newFixedThreadPool(8)
+  val pool = FuturePool(executor)
 
-  def delayedRun(n: Int): Future[Int] = {
-    Future.value({
-      Thread.sleep(10000)
-      n + 1
-    })
+  def delayedRun(n: Int): Int = {
+    Thread.sleep(10000)
+    n + 1
   }
+
   println("creating a promise...")
   val promise = new Promise[Int]
   println("creating the future on the promise")
-  val future = promise map(delayedRun)
+  val future = pool { promise map(delayedRun) }
+  println("future = " + future.toString)
   println("we'll set a value on the promise...")
   promise setValue(100)
   println("A value have already been sent and we'll setup the callback")
@@ -22,6 +26,8 @@ object Test extends App {
   println("Callback ok and now we'll wait for the future...")
 
   Await.ready(future)
-
-  println("ending...")
+  Thread.sleep(1)
+  println("ending... shutting down executor service")
+  executor.shutdown()
+  System.exit(0)
 }
--- spark-finagle/src/main/scala/me/dan/spark/WordCountExecutor.scala
+++ spark-finagle/src/main/scala/me/dan/spark/WordCountExecutor.scala
@@ -1,11 +1,27 @@
 package me.dan.spark
 
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.rdd.{PairRDDFunctions, RDD}
 
 import collection.mutable.{HashMap, SynchronizedMap}
 
+import com.twitter.util.{Await, Future, FuturePool, Promise}
+
+import java.util.concurrent.{Future => JFuture, _}
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.{PairRDDFunctions, RDD}
+
+
+
 object WordCountExecutor {
+
+  lazy val executor = {
+    val execs = Executors.newFixedThreadPool(8)
+    sys.addShutdownHook({
+      execs.shutdown
+    })
+    execs
+  }
+  lazy val pool = FuturePool(executor)
 
   case class Content(id: Int, content: String)
   abstract class Status(val name: String)
@@ -13,7 +29,6 @@ object WordCountExecutor {
   case object Invalid extends Status("invalid")
   case object Running extends Status("running")
   case object Successful extends Status("successful")
-
 
   private var counter: Int = 0
 
@@ -51,9 +66,18 @@ object WordCountExecutor {
     sc.textFile("/tmp/word_count/out_" + id).collect.toList
 
 
+
   def process(jobId: Int, contentIds: List[Int]) {
-    try {
-      statuses(jobId) = Running
+    statuses(jobId) = Running
+    val promise = new Promise[(Int, List[Int])]
+    val future = pool { promise map(kv => run(kv._1, kv._2)) }
+    future.onSuccess { n => statuses(jobId) = Successful }
+    future.onFailure { case e: Exception => statuses(jobId) = Failed }
+    promise setValue((jobId, contentIds))
+  }
+
+  def run(jobId: Int, contentIds: List[Int]) {
+    Future.value({
       val rdd: RDD[String] = contentIds map(id => sc.textFile("/tmp/word_count/in_" + id + ".txt")) reduce(_ union _)
       rdd
         .flatMap(_.split("\\s+"))
@@ -61,14 +85,7 @@ object WordCountExecutor {
         .reduceByKey(_ + _)
         .map(kv => kv._1 + "|" + kv._2)
         .saveAsTextFile("/tmp/word_count/out_" + id)
-      statuses(jobId) = Successful
-    }
-    catch {
-      case e: Exception => {
-        println(e.getMessage)
-        statuses(jobId) = Failed
-      }
-    }
+    })
   }
 
   def status(id: Int) = statuses getOrElse(id, Invalid)
