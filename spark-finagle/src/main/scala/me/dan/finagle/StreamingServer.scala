8ff46cb05f696f92554c45976a343dca6b2618f2
dqsgit <kernelonsteroids@gmail.com>
2016-11-29 06:52:38 +0800

    updated scala version to 2.11 for spark-finagle/ added postStream on StreamingServer.scala

--- spark-finagle/src/main/scala/me/dan/finagle/StreamingServer.scala
+++ spark-finagle/src/main/scala/me/dan/finagle/StreamingServer.scala
@@ -2,9 +2,11 @@ package me.dan.finagle
 
 import com.twitter.concurrent.AsyncStream
 import com.twitter.conversions.time._
-import com.twitter.finagle.http.{Request, Response, Status}
 import com.twitter.finagle.{Http, Service}
-import com.twitter.io.{Buf, Reader}
+import com.twitter.finagle.http.{Request, Response, Status}
+import com.twitter.finagle.http.path._
+import com.twitter.finagle.http.Method
+import com.twitter.io.{Buf, BufInputStream, Reader}
 import com.twitter.util.{Await, Future, JavaTimer}
 
 
@@ -17,55 +19,65 @@ import scala.util.Random
  * An example of a streaming HTTP server using chunked transfer encoding.
  */
 object StreamingServer {
-  val random = new Random
-  implicit val timer = new JavaTimer
 
-  // Int, sleep, repeat.
-  /*
-  def ints(): AsyncStream[Int] =
-    random.nextInt +::
-      AsyncStream.fromFuture(Future.sleep(100.millis)).flatMap(_ => ints())
-  */
+  def getStream = new Service[Request, Response] {
+
+    val itr = Source.fromFile(new File("pom.xml")).getLines
+
+    def getNext: Future[Option[String]] = Future.value({
+      if(itr.hasNext) Some(itr.next)
+      else None
+    })
+
+    def mkStream: AsyncStream[Option[String]] = AsyncStream.fromFuture(getNext) ++ mkStream
+
+    def messages: AsyncStream[Buf] = mkStream takeWhile(_ isDefined) map(l => Buf.Utf8(l.get + "\n"))
+
+    def apply(request: Request): Future[Response] = {
+      val writable = Reader.writable()
+      // Start writing thread.
+      messages
+        .foreachF(buf => writable.write(buf))
+        .onSuccess {
+          r => writable
+            .close()
+            .ensure { println("everthing has been read") }
+        }
+
+      Future.value(Response(request.version, Status.Ok, writable))
+    }
+  }
+
+
+  def postStream = new Service[Request, Response] {
+      def apply(request: Request): Future[Response] = {
+        def doAndReturn = {
+            // read(1024) foreach(ob => ob foreach(println))
+            AsyncStream.fromReader(request.reader) foreach(buf => Source.fromInputStream(new BufInputStream(buf)).getLines.foreach(println))
+            Response(request.version, Status.Created)
+        }
+        val ret = Future.value(doAndReturn)
+        println("already returned")
+        ret
+      }
+  }
+
+  val services = HttpRouter.byRequest { request =>
+   (request.method, Path(request.path)) match {
+     case Method.Get  -> Root => getStream
+     case Method.Post -> Root => postStream
+   }
+ }
+
 
   def main(args: Array[String]): Unit = {
-    def service = new Service[Request, Response] {
 
-      val itr = Source.fromFile(new File("pom.xml")).getLines.toIterator
-
-      def getNext: Future[Option[String]] = Future.value({
-        if(itr.hasNext) Some(itr.next)
-        else None
-      })
-
-      def mkStream: AsyncStream[Option[String]] = AsyncStream.fromFuture(getNext) ++ mkStream
-
-      // Only one stream exists.
-      // @volatile private[this]
-      val messages: AsyncStream[Buf] = mkStream takeWhile(_ isDefined) map(l => Buf.Utf8(l.get + "\n"))
-
-      // Allow the head of the stream to be collected.
-      //messages.foreach(_ => messages = messages.drop(1))
-
-      def apply(request: Request): Future[Response] = {
-        val writable = Reader.writable()
-        // Start writing thread.
-        messages
-          .foreachF(buf => writable.write(buf))
-          .onSuccess {
-            r => writable
-              .close()
-              .ensure { println("everthing has been read") }
-          }
-
-        Future.value(Response(request.version, Status.Ok, writable))
-      }
-    }
 
     Await.result(Http.server
       // Translate buffered writes into HTTP chunks.
       .withStreaming(enabled = true)
       // Listen on port 8080.
-      .serve("0.0.0.0:58888", service)
+      .serve("0.0.0.0:58888", services)
     )
   }
 }
